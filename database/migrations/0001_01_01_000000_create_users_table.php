d9ac374814b2a32b0a53b8c1e599b971f5746249
elyasdehati <elyas786dehaty@gmail.com>
2026-04-05 11:01:10 +0430

    Breeze Installed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->boolean('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
