6f866d4abde6c3c76dca6ef50dc34addaedbfeb0
elyasdehati <elyas786dehaty@gmail.com>
2026-06-25 21:27:10 +0430

    User Activity Logger functionality added in the Training Type Catalog

--- app/Http/Controllers/TrainingTypeController.php
+++ app/Http/Controllers/TrainingTypeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TrainingType;
 use Illuminate\Http\Request;
+use App\Services\ActivityLogger;
 
 class TrainingTypeController extends Controller
 {
@@ -21,6 +22,11 @@ class TrainingTypeController extends Controller
             'name' => $request->name
         ]);
 
+        ActivityLogger::log(
+            'create_training_type',
+            'Training type created: ' . $request->name
+        );
+
         return redirect()->route('all.training.type');
     }
 
@@ -36,11 +42,22 @@ class TrainingTypeController extends Controller
             'name' => $request->name
         ]);
 
+        ActivityLogger::log(
+            'update_training_type',
+            'Training type updated ID: ' . $id
+        );
+
         return redirect()->route('all.training.type');
     }
 
     public function DeleteTrainingType($id){
         $all_types = TrainingType::findOrFail($id);
+
+        ActivityLogger::log(
+            'delete_training_type',
+            'Training type deleted ID: ' . $id
+        );
+
         $all_types->delete();
 
         return redirect()->route('all.training.type');
